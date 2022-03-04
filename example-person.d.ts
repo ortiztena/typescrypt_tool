da034162f8dcd776f4dfe63f13e2b3d118c31ecd
ortiztena <ortiztena@gmail.com>
2022-03-04 12:19:59 +0100

    test demo

--- example-person.d.ts
+++ example-person.d.ts
@@ -6,11 +6,5 @@
  */
 
 export interface ExampleSchema {
-  firstName: string;
-  lastName: string;
-  /**
-   * Age in years
-   */
-  age?: number;
-  hairColor?: "black" | "brown" | "blue";
+  [k: string]: unknown;
 }
